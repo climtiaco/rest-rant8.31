ad98ff9c37aa1313122fd6688d44e17188c95352
Christian Limtiaco <climtiaco.code@gmail.com>
2023-09-19 21:37:20 -0700

    added css style but my button doesnt pick up the cdn

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ console.log(process.env)
 const express = require('express')
 const app = express()
 
+
+//Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+
+//This is where the react router is connected to the separate js file.
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -16,5 +22,7 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-console.log(process.env.PORT)
+
 app.listen(process.env.PORT)
+ 
+// Inside the gitignore is node_modules / .env / .DS_Store... the env file just has the port number which you could change accordingly.
