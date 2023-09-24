d6bd38150972ce5e5e4b12b2497166525e5fb044
Christian Limtiaco <climtiaco.code@gmail.com>
2023-09-24 15:10:58 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 const places = require('../models/places')
 
 //INDEX
+// Res.render(view, [locals], [callback])... so this one specifically sets the route up for the places/index route which the 'views' engine is set up to show. And {places} would be the locals that it's pulling from the api.
 router.get('/', (req, res) => {     
     res.render('places/index', { places })
 })
 
 //POST
+// Req.body is in relation to the form that we're submitting. And its a bunch of if statements that have the "!" to signify a falsy boolean. So it's saying if it's not there, than place the pic I'm asking it to do.
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
@@ -19,20 +21,31 @@ router.post('/', (req, res) => {
       req.body.state = 'USA'
     }
     places.push(req.body)
-    res.redirect('POST /places')
+    res.redirect('/places')
 })
 
 //NEW
+//res.redirect([status,] path)
+// The whole form is on new.jsx and we're literally just creating the route for it here. 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 //SHOW
 router.get('/:id', (req, res) => {
-  if (places[req.params.id]) {
-    res.render('')
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+
   }
 })
+
 
 
 
