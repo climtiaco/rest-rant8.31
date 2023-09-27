5de759a0a5e791812211c13ef509a5b5fc3ca636
Christian Limtiaco <climtiaco.code@gmail.com>
2023-09-26 21:36:32 -0700

    Finished creating the delete button & its functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,6 +1,12 @@
 const React = require('react')
 const Def = require('../default')
 
-function edit_form() {}
+function edit_form() {
+    return (
+        <Def>
+            
+        </Def>
+    )
+}
 
 module.exports = edit_form
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,6 +6,7 @@ function index (data) {
       return (
         <div className="col-sm-6">
           <h2>
+            {/* show route */}
             <a href={`/places/${index}`} >
             {place.name}
             </a>
